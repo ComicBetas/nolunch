aeee325bc26d0f85a33d242fee227572933350a9
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-03 12:46:00 -0700

    renamed product to donate

--- donate.php
+++ donate.php
@@ -4,14 +4,14 @@ Including partial files most Content Management Systems (CMSs) divide out
 */
 include("assets/includes/global-header.php"); // HTML head ?>
 <?php include("assets/includes/main-header.php"); // Visible header ?>
-<div id="product" class="container">
+<div id="donate" class="container">
   <div id="content">
     <div id="main_content">
-      <h1>product</h1>
+      <h1>donate</h1>
       Hello World <a href="#">test</a>
     </div><!-- #main_content -->
     <?php include("assets/includes/sub-nav.php"); // An optional sub navigation ?>
   </div><!-- #content -->
   <?php include("assets/includes/main-footer.php"); // Visible footer ?>
-</div><!-- #product -->
+</div><!-- #donate -->
 <?php include("assets/includes/global-footer.php"); // HTML foot ?>
