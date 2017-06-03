5f73e8eaf2e24f5fe8311aae3a8e50ff7162dd5e
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-03 12:36:00 -0700

    revised the links in the main nav

--- assets/includes/fragments/main-nav.php
+++ assets/includes/fragments/main-nav.php
@@ -1,8 +1,7 @@
 <ul class="nav navbar-nav">
   <li><a href="index.php">home</a></li>
-  <li><a href="#">shop</a></li>
-  <li><a href="product.php">product</a></li>
   <li><a href="about.php">about</a></li>
+  <li><a href="donate.php">donate</a></li>
   <li><a href="contact.php">contact</a></li>
   <li><a href="#">blog</a></li>
 </ul>
