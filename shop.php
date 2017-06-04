4a2168d6dd0060184b777ebe54e9e57277a500bc
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-03 21:38:14 -0700

    shop page addition

---
+++ shop.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Including partial files most Content Management Systems (CMSs) divide out
+*/
+include("assets/includes/global-header.php"); // HTML head ?>
+<?php include("assets/includes/main-header.php"); // Visible header ?>
+<div id="shop" class="container">
+  <div id="content">
+    <div id="main_content">
+      <h1>shop</h1>
+      Hello World <a href="#">test</a>
+    </div><!-- #main_content -->
+    <?php include("assets/includes/sub-nav.php"); // An optional sub navigation ?>
+  </div><!-- #content -->
+  <?php include("assets/includes/main-footer.php"); // Visible footer ?>
+</div><!-- #shop -->
+<?php include("assets/includes/global-footer.php"); // HTML foot ?>
