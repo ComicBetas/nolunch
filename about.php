44c3b62590aa9938a58bf728037b83ad60eb9418
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-04 16:58:10 -0700

    adding about page content

--- about.php
+++ about.php
@@ -8,7 +8,15 @@ include("assets/includes/global-header.php"); // HTML head ?>
   <div id="content">
     <div id="main_content">
       <h1>about</h1>
-      Hello World <a href="#">test</a>
+      <img src="assets/images/1431892117.jpg">
+      <p>Katrina and Tadashi have been traveling the world since 2004, mainly to Southeast Asia.  They travel flexibly, and if they don't like a place, they'll leave early, and if they fall in love with a place, they'll stay longer.  They strive to meet the local people, and so they tend to stay away from the big cities and hotels, preferring smaller places where it's easier to make connections.  In this way, they have made friends all over the world, and have helped bring running water to people's house, payed for many children to attend school, helped an adult finish their education, and helped start two small businesses.  They see themselves as providing the support that student loans have given so many of us in the United States.  Except instead of being payed back, the people they support pay it forward.  Their first book, "<a href="/" title="">No Lunch, No Money, No Rice: The Pursuit of Education in Asia</a>" was a labor of love, and is filled with the stories they would tell friends when they asked, "How was your trip?"</p>
+      <h2>Katrina Keating</h2>
+      <p>Katrina is a mathematics professor at Diablo Valley College in Pleasant Hill, California.  She has a Masters in Mathematics, but was an anthropology major until her last year of undergraduate work.  Studying anthropology opened her eyes and instilled a passion for cultures and a need  for travel. Traveling the world, mostly in Southeast Asia, has taught her a respect for how precarious and precious life and education are, and she is determined to help raise awareness.</p>
+      <a href="http://katrinakphotography.blogspot.com/2009/03/parahawking.html" class="btn btn-primary" target="_blank">Katrina's Blog</a>
+      <h2>Tadashi Tsuchida</h2>
+      <p>Tadashi is a mathematics professor at Skyline College in San Bruno, California.  He has a Masters in Mathematics, but has always had a passion for photography and is rarely seen without his camera.  As a young boy, he and his father would take road trips on every holiday, giving him an appreciation for travel.  He lives with his wife, Katrina, in Oakland, California.</p>
+      <a href="http://tadashiphotography.blogspot.com" class="btn btn-primary" target="_blank">Tadashi's Blog</a>
+      <a href="http://tadashifinephoto.com" class="btn btn-primary" target="_blank">Tadashi's Photos</a>
     </div><!-- #main_content -->
     <?php include("assets/includes/sub-nav.php"); // An optional sub navigation ?>
   </div><!-- #content -->
