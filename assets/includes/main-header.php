f232ea0f133ad12d755407d7922394a383932890
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-16 13:58:56 -0700

    initial header formatting

--- assets/includes/main-header.php
+++ assets/includes/main-header.php
@@ -1,6 +1,6 @@
-<header class="navbar navbar-default navbar-fixed-top" role="navigation">
+<header id="site_navbar" class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container">
-    <a class="navbar-brand" href="index.php">noLunch</a>
+    <a class="navbar-brand" href="index.php"><img src="/assets/images/logo.png" alt="noLunch"></a>
     <nav class="navbar-header">
       <?php include("assets/includes/fragments/main-nav.php"); // The Main Menu ?>
     </nav>
