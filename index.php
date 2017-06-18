d39cd2f468d19166c1012aa0d659134b6edd1307
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-17 20:04:05 -0700

    homepage SRC

--- index.php
+++ index.php
@@ -7,9 +7,20 @@ include("assets/includes/global-header.php"); // HTML head ?>
 <div id="homepage" class="container">
   <div id="content">
     <div id="main_content">
-      Hello World <a href="#">test</a>
+      <h2>Welcome</h2>
+      <p>We are passionate about travel, and educating people about what we have learned in Asia over the last decade.  We are photographers, writers, and philanthropists.</p>
+      <a href="photographs.php" class="btn btn-default">See Photographs</a>
+      <a href="shop.php" class="btn btn-default">Shop Now</a>
+      <hr>
+      <p>Our first book, <em>No Lunch, No Money, No Rice: The Pursuit of Education in Asia</em> is about education in Asia, but not through statistics, through personal narratives and photographs.   It tells people’s stories—in their own words—of getting an education in Asia.  Some of these stories are profound and inspiring, while others are funny.  Many are both sad and enlightening.</p>
+      <figure>
+        <blockquote>When I was a girl, I asked my father if I can go to school and he say to me, 'We have no lunch, no money, no rice, no pig, so you cannot go to school'.</blockquote>
+        <figcaption>Xue, age 22, farmer, Sapa, Vietnam</figcaption>
+      </figure>
+      <hr>
+      <h2>Preview <small>No Lunch, No Money, No Rice: The Pursuit of Education in Asia</small></h2>
+      <p><div>In the preview, you can read the <u class="alert">introduction</u>, the <u class="alert">first chapter</u> (Nepal), the <u class="alert">afterword</u>, <u class="alert">How To Help</u> &amp; <u>suggested readings</u>. <em>Unfortunately, the preview does not work well on mobile devices. We apologize for the inconvenience.</em></p>
     </div><!-- #main_content -->
-    <?php include("assets/includes/sub-nav.php"); // An optional sub navigation ?>
   </div><!-- #content -->
   <?php include("assets/includes/main-footer.php"); // Visible footer ?>
 </div><!-- #homepage -->
