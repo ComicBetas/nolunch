7a1ac9769a6729ee0fcd092b5308a846285655c2
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-06-27 17:07:21 -0700

    brought the book more into focus on the homepage

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ include("assets/includes/global-header.php"); // HTML head ?>
       <a href="photographs.php" class="btn btn-default">See Photographs</a>
       <a href="shop.php" class="btn btn-default">Shop Now</a>
       <hr>
+      <h2>Preview <small>No Lunch, No Money, No Rice: The Pursuit of Education in Asia</small></h2>
+      <p><div>In the preview, you can read the <u class="alert">introduction</u>, the <u class="alert">first chapter</u> (Nepal), the <u class="alert">afterword</u>, <u class="alert">How To Help</u> &amp; <u>suggested readings</u>. <em>Unfortunately, the preview does not work well on mobile devices. We apologize for the inconvenience.</em></p>
+      <hr>
       <p>Our first book, <em>No Lunch, No Money, No Rice: The Pursuit of Education in Asia</em> is about education in Asia, but not through statistics, through personal narratives and photographs.   It tells people’s stories—in their own words—of getting an education in Asia.  Some of these stories are profound and inspiring, while others are funny.  Many are both sad and enlightening.</p>
       <figure>
         <blockquote>When I was a girl, I asked my father if I can go to school and he say to me, 'We have no lunch, no money, no rice, no pig, so you cannot go to school'.</blockquote>
         <figcaption>Xue, age 22, farmer, Sapa, Vietnam</figcaption>
-      </figure>
-      <hr>
-      <h2>Preview <small>No Lunch, No Money, No Rice: The Pursuit of Education in Asia</small></h2>
-      <p><div>In the preview, you can read the <u class="alert">introduction</u>, the <u class="alert">first chapter</u> (Nepal), the <u class="alert">afterword</u>, <u class="alert">How To Help</u> &amp; <u>suggested readings</u>. <em>Unfortunately, the preview does not work well on mobile devices. We apologize for the inconvenience.</em></p>
+      </figure>    
     </div><!-- #main_content -->
   </div><!-- #content -->
   <?php include("assets/includes/main-footer.php"); // Visible footer ?>
